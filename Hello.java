5588919029ad3c4d462834b57d4ae8350957c4dc
InnovativeSumit <pal2024santosh@gmail.com>
2026-07-01 07:36:23 +0530

    Basic PrintOut

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        // System.out.print("Hello");
+        // System.out.print("Java in VS Code!");
+        // System.out.println("Hello Java in VS Code!");
+
+        System.out.println("Hello");
+        System.out.println("Java in VS Code!");
+        System.out.print("Hello! Java in VS Code!");
+
+
+    }
+}
