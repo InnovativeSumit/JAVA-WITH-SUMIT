ab2cfd8634e11805444f4081e8eb3036dc8370c8
InnovativeSumit <pal2024santosh@gmail.com>
2026-07-01 07:46:45 +0530

    psvm and sout

--- Hello.java
+++ Hello.java
@@ -1,4 +1,5 @@
 public class Hello {
+    // Shortcut is ===> psvm
     public static void main(String[] args) {
         // System.out.print("Hello");
         // System.out.print("Java in VS Code!");
@@ -8,6 +9,22 @@ public class Hello {
         System.out.println("Java in VS Code!");
         System.out.print("Hello! Java in VS Code!");
 
+        /// Shortcut is ===> sout for
+        // System.out.println();
+
+        // Right Half Pyramid
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        // Reverse Right half Pyramid
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
 
     }
 }
